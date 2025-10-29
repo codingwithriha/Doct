f86ba9a00107de4ee11d0f229cdbe527d6e6f351
Codingwithriha <rihashehzadi2003@gmail.com>
2025-10-29 13:02:08 +0500

    feat: complete Contact us page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,12 @@ import { Route, Routes } from 'react-router-dom'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
-import Contacts from './pages/Contacts'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
@@ -21,7 +21,7 @@ const App = () => {
         <Route path='/doctors/:speciality' element={<Doctors />} />
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
-        <Route path='/contacts' element={<Contacts />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
