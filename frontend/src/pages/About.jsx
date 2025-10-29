464aab8cad01e7b9c2072aa19a827e98f6e4cf26
Codingwithriha <rihashehzadi2003@gmail.com>
2025-10-29 13:21:19 +0500

    update border

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -49,21 +49,21 @@ const About = () => {
           </p>
 
           <div className="flex flex-col md:flex-row mb-20 mt-4">
-            <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border border-gray-300 px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
               <b>Efficiency:</b>
               <p>
                 Streamlined appointment scheduling that fits into your busy
                 lifestyle.
               </p>
             </div>
-            <div className="border px-10 md:px-15 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border border-gray-300 px-10 md:px-15 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
               <b>Convenience:</b>
               <p>
                 Access to a network of trusted healthcare professionals in your
                 area.
               </p>
             </div>
-            <div className="border px-10 md:px-15 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+            <div className="border border-gray-300 px-10 md:px-15 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
               <b>Personalization:</b>
               <p>
                 Tailored recommendations and reminders to help you stay on top
