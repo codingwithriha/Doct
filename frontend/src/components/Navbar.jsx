18639c6296428d451138cf83eb253199770359f8
Codingwithriha <rihashehzadi2003@gmail.com>
2025-10-29 13:06:18 +0500

    feat: comple create account UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,19 +12,19 @@ const Navbar = () => {
       <ul className="hidden md:flex items-start gap-5 font-medium ">
         <NavLink to="/">
           <li className="py-1 ">HOME</li>
-          <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden" />
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/doctors">
           <li className="py-1 ">ALL DOCTORS</li>
-          <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden" />
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/about">
           <li className="py-1 ">ABOUT</li>
-          <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden" />
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/contact">
           <li className="py-1 ">CONTACT</li>
-          <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden" />
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
@@ -44,7 +44,7 @@ const Navbar = () => {
           :
           <button
             onClick={() => navigate("/login")}
-            className="bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block"
+            className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
           >
             Create account
           </button>
